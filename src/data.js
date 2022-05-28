fb5582b54ae188e8a10a4f7d87eb5f64455e4290
anamkaa <anamikakashyap99@gmail.com>
2022-05-28 23:14:41 +0530

    imported images

---
+++ src/data.js
@@ -0,0 +1,6 @@
+const DATA = Object.freeze({
+   CYCLE : "https://greenalley.netlify.app/assets/cycle-no-bg.png",
+  DEALS : "https://greenalley.netlify.app/assets/deal-pl.jpg",
+});
+
+export default DATA;
