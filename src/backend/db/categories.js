e56af1746fe583f4916ef7f0741d6309861934f3
anamkaa <anamikakashyap99@gmail.com>
2022-05-28 23:15:57 +0530

    updated database

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,24 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
+    categoryName: "Grown-Plants",
     description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+      "",
+      imgSrc: "https://greenalley.netlify.app/assets/grown-pl.jpg"
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "Plant-Pots",
     description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+      "",
+      imgSrc: "https://greenalley.netlify.app/assets/pot-pl.jpg"
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "Garden-Tools",
     description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+      "",
+      imgSrc: "https://greenalley.netlify.app/assets/tools.jpg"
   },
 ];
+
