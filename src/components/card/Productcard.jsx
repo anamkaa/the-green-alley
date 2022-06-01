1bb19105b06373e9dd9a7582ae1c2231085a8a93
anamkaa <anamikakashyap99@gmail.com>
2022-06-01 21:02:27 +0530

    made changes in product card

--- src/components/card/Productcard.jsx
+++ src/components/card/Productcard.jsx
@@ -1,38 +1,56 @@
-import React from 'react';
-import { useCart } from '../../context/cart-context';
-import { useWishlist } from '../../context/wishlist-context';
+import React from "react";
+import { useCart } from "../../context/cart-context";
+import { useWishlist } from "../../context/wishlist-context";
 
-const Productcard = ({product}) => {
-
-  const {addToCart} = useCart();
-  const {addToWishlist} = useWishlist();
+const Productcard = ({ product }) => {
+  const { addToCart } = useCart();
+  const { addToWishlist } = useWishlist();
   return (
-   <>
-   <div className="gh-card card card-image shadow">
+    <>
+      <div className="gh-card card card-image shadow">
         <div className="card-image-container">
           <i className="ph-heart card-icon-heart gh-heart"></i>
-          <img src={product.imgSrc} alt="product-image" className="card-image" />
+          <img
+            src={product.imgSrc}
+            alt="product-image"
+            className="card-image"
+          />
           <div className="card-rating">
             <span className="rating">{product.rating}</span>
             <i className="fas fa-star card-icon-star"></i> | {product.review}
           </div>
         </div>
-        <div className="card-content" >
-          <div className="gh-card-title card-image-content-title">{product.name}</div>
-    
+        <div className="card-content">
+          <div className="gh-card-title card-image-content-title">
+            {product.name}
+          </div>
+
           <div className="price-text">
-          <del className="card-text-red">{product.price}</del>
-          <span style={{fontSize:"1.2rem"}}>{product.discountPrice}</span>
-          <span>{product.off}</span>
+            <del className="card-text-red">{product.price}</del>
+            <span style={{ fontSize: "1.2rem" }}>{product.discountPrice}</span>
+            <span>{product.off}</span>
           </div>
         </div>
         <div className="card-btn-container gh-btn-container">
-          <button className="card-btn gh-btn gh-btn-primary" onClick={()=>addToCart()}>Add to Cart</button>
-          <button className="card-btn gh-btn gh-btn-secondary" onClick={()=>addToWishlist()}>Wishlist</button>
+          <button
+            className="card-btn gh-btn gh-btn-primary"
+            onClick={() => addToCart()}
+            disabled={!product.inStock}
+            style={{ opacity: !product.inStock && "0.5" }}
+          >
+            {product.inStock === true ? "Add to cart" : "out of stock"}
+          </button>
+
+          <button
+            className="card-btn gh-btn gh-btn-secondary"
+            onClick={() => addToWishlist()}
+          >
+            Wishlist
+          </button>
         </div>
       </div>
-   </>
-  )
-}
+    </>
+  );
+};
 
 export default Productcard;
