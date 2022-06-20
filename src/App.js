19023aaaa12cb98ab44eee589eac591f38dbfa23
anamkaa <anamikakashyap99@gmail.com>
2022-06-20 23:38:25 +0530

    created and imported require-auth

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import "./App.css";
-import {Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Productlisting from "./pages/Productlisting";
-import Wishlist from './pages/Wishlist';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Cartmanagement from './pages/Cartmanagement';
-// import Categorypage from "./context/category-context";
+import Wishlist from "./pages/Wishlist";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Cartmanagement from "./pages/Cartmanagement";
+import { RequireAuth } from "./components/require-auth";
 
 function App() {
   return (
     <div className="App">
-       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/wishlist" element={<Wishlist/>}/>
-        <Route path="/productlisting/:categoryName" element={<Productlisting/>}/>
-        <Route path="/cartmanagement" element={<Cartmanagement/>}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/wishlist"
+          element={
+            <RequireAuth>
+              <Wishlist />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/productlisting/:categoryName"
+          element={<Productlisting />}
+        />
+        <Route
+          path="/cartmanagement"
+          element={
+            <RequireAuth>
+              <Cartmanagement />
+            </RequireAuth>
+          }
+        />
       </Routes>
-     {/* <Home/> */}
-     {/* <Categorypage/> */}
-    
     </div>
   );
 }
 
 export default App;
-
